22574599f11170e337bcdcfba5e4e6694b2d89b9
Todd Wolfson <todd@twolfson.com>
2014-11-07 22:47:31 -0800

    Updated tests to match

--- test/foundry-release-git-semver-branches-test.js
+++ test/foundry-release-git-semver-branches-test.js
@@ -8,50 +8,6 @@ var wrench = require('wrench');
 var gitRelease = require('../');
 var fixtureUtils = require('./utils/fixtures');
 
-describe('Committing', function () {
-  describe('in a git folder', function () {
-    fixtureUtils.bundle.mkdir('commit_test');
-    fixtureUtils.bundle.exec('git init');
-    fixtureUtils.bundle.exec('touch a');
-    fixtureUtils.bundle.exec('git add -A');
-    fixtureUtils.bundle.exec('git commit -m "Initial commit =D"');
-
-    before(function commit (done) {
-      this.inBundle(function () {
-        gitRelease.commit({
-          version: '0.1.0',
-          message: 'Release 0.1.0',
-          description: null
-        }, done);
-      });
-    });
-
-    it('adds a git tag', function (done) {
-      this.inBundle(function () {
-        exec('git tag', function (err, stdout, stderr) {
-          if (err) {
-            return done(err);
-          }
-          expect(stdout).to.equal('0.1.0\n');
-          done();
-        });
-      });
-    });
-
-    it('adds a git commit', function (done) {
-      this.inBundle(function () {
-        exec('git log --format=oneline -n 1', function (err, stdout, stderr) {
-          if (err) {
-            return done(err);
-          }
-          expect(stdout).to.match(/\w{32} Release 0.1.0\n/);
-          done();
-        });
-      });
-    });
-  });
-});
-
 describe('Publishing', function () {
   describe('in a git folder', function () {
     fixtureUtils.bundle.mkdir('publish_test');
@@ -71,18 +27,23 @@ describe('Publishing', function () {
     before(function publish (done) {
       this.inBundle(function () {
         gitRelease.publish({
-          version: '0.1.0',
-          message: 'Release 0.1.0',
+          version: '1.0.0',
+          message: 'Release 1.0.0',
           description: null
         }, done);
       });
     });
 
-    it('publishes the commit', function () {
-      expect(this.execStub.args[0]).to.deep.equal(['git push']);
+    it('updates and publishes the minor variable branch', function () {
+      expect(this.execStub.args[0]).to.deep.equal(['git checkout -B 1.x.x']);
+      expect(this.execStub.args[1]).to.deep.equal(['git push']);
+      expect(this.execStub.args[2]).to.deep.equal(['git checkout -']);
     });
+
     it('publishes the tags', function () {
-      expect(this.execStub.args[1]).to.deep.equal(['git push --tags']);
+      expect(this.execStub.args[3]).to.deep.equal(['git checkout -B 1.0.x']);
+      expect(this.execStub.args[4]).to.deep.equal(['git push']);
+      expect(this.execStub.args[5]).to.deep.equal(['git checkout -']);
     });
   });
 });
